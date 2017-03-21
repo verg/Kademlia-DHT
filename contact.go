887192a5ad8184e14d4bab3e9447c6d642fc725c
Ryan Vergeront <ryanvergeront@gmail.com>
2017-03-20 19:13:11 -0500

    Add Port and sorting to Contact

--- contact.go
+++ contact.go
@@ -5,6 +5,7 @@ import "net"
 type Contact struct {
 	ID   NodeID
 	Addr net.IP
+	Port uint16
 }
 
 func NewContact(id NodeID, addr net.IP) Contact {
@@ -13,3 +14,9 @@ func NewContact(id NodeID, addr net.IP) Contact {
 		Addr: addr,
 	}
 }
+
+type Contacts []Contact
+
+func (c Contacts) Len() int           { return len(c) }
+func (c Contacts) Less(i, j int) bool { return c[i].ID.Less(c[j].ID) }
+func (c Contacts) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
